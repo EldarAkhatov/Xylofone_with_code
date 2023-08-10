beee7c0f12702cdf869acdabe0e3fb3c8adf2ba8
Eldar Akhatov <eldar_ahatov@icloud.com>
2023-08-10 17:57:35 +0300

    prepare project to create UI with code. Add sounds

---
+++ Xylophone_with_code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xylophone_with_code
+//
+//  Created by Эльдар Ахатов on 10/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
